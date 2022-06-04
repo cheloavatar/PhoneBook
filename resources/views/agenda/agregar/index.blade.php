0ef7d9eb5132a217c72715774e59df3f01734e08
udpc.colima <udpc.colima@gmail.com>
2022-06-04 14:37:41 -0500

    Traducir

--- resources/views/agenda/agregar/index.blade.php
+++ resources/views/agenda/agregar/index.blade.php
@@ -22,7 +22,7 @@
                     <button type="button" onclick="window.location='{{ route('agenda.create') }}'" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                             <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" />
-                        </svg> <span class="px-2"> AGREGAR DETENIDO </span>
+                        </svg> <span class="px-2"> AGREGAR PERSONA </span>
                     </button>
                     </div>
                     <table class="table-auto w-full">
